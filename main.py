036eade64475c3d094dc9beb3b157f0094c4c385
Enzo <enxo09@gmail.com>
2017-05-25 16:20:28 -0600

    Second commit

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ apellidos=['escalante','campos','reyes','rodriguez','portillo'];
 
 contador=1;
 
-for contador in nombres:
-    print(contador);
+for contador in apellidos:
+    for contador2 in nombres:
+        nombreCompleto=[contador+contador2];
+
+for cont in nombreCompleto:
+    print(nombreCompleto);
+
+print('\nGracias por usar mi aplicacion!');
